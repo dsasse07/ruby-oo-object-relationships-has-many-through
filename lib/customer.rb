63ed45948107ca6ed477661e83a5ec70972dbca0
Daniel Sasse <dsasse07@gmail.com>
2020-12-10 23:36:58 -0500

    Done

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,36 @@
+require 'pry'
+require_relative 'meal'
+require_relative 'waiter'
+
 class Customer
-  
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, cost, tip)
+        Meal.new(waiter, self, cost, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end
+
+    def waiters
+        meals.map do |meal|
+            meal.waiter
+        end
+    end 
+
 end
---
+++ lib/run.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require_relative 'meal'
+require_relative 'waiter'
+require_relative 'customer'
+
+
+
